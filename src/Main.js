d1e55e696e0eedc0c941332a1f883182cd58e4d2
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-07 13:00:54 +0100

    Add MapView to Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+
 
 const Main = () => {
     return (
-        <View>
-            <Text>Opppsss</Text>
+        <View style={{ flex: 1 }}>
+            <MapView
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
         </View>
     )
 }
