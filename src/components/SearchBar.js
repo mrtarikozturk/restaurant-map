ebe08e4f893f353d7ebdb2dff36b041c4ecfdc6a
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-07 13:52:12 +0100

    Add SeacrhBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, TextInput } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const SearchBar = () => {
+//My Imports
+import { searchBar } from '../style';
+
+const SearchBar = (props) => {
     return (
-        <View>
-            <Text>Search Bar</Text>
+        <View style={searchBar.container}>
+            <Icon name='search' size={20} color='gray' />
+            <TextInput
+                style={searchBar.input}
+                placeholder='Search a city..'
+                onChangeText={(text) => props.onChangeText(text)}
+            />
         </View>
     )
 }
 
-export { SearchBar };
+export { SearchBar }
