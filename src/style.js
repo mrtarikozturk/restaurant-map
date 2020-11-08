3c0c978965b3d6ba736925aa4bed438e7f3fc555
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-08 13:17:47 +0100

    Add City component style

--- src/style.js
+++ src/style.js
@@ -18,3 +18,26 @@ export const searchBar = StyleSheet.create({
     }
 
 })
+
+// City Component
+export const city = StyleSheet.create({
+    container: {
+        backgroundColor: '#ffd54f',
+        padding: 5,
+        margin: 10,
+        marginTop: 20,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#e0e0e0',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#424242',
+        fontWeight: 'bold',
+        marginLeft: 5
+    }
+
+})
+
+
