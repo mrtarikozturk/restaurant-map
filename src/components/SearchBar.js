5939a14a3cbcbfe1ce20cd93ddff468c7aab74dd
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-08 13:18:35 +0100

    Import materialCommunityIcons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, TextInput } from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 //My Imports
 import { searchBar } from '../style';
@@ -8,7 +8,7 @@ import { searchBar } from '../style';
 const SearchBar = (props) => {
     return (
         <View style={searchBar.container}>
-            <Icon name='search' size={20} color='gray' />
+            <Icon name='magnify' size={20} color='gray' />
             <TextInput
                 style={searchBar.input}
                 placeholder='Search a city..'
